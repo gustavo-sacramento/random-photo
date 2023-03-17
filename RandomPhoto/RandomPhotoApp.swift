7ad21548c70b90c07efe15dbe1201d380e3650a3
Gustavo Sacramento <gustavo.saramento211@gmail.com>
2023-03-17 09:44:43 -0300

    Initial Commit

---
+++ RandomPhoto/RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoApp.swift
+//  RandomPhoto
+//
+//  Created by Gustavo Sacramento on 17/03/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
