9b853fc91865d2eb71e5a5b30248d935244ed61a
Gustavo Sacramento <gustavo.saramento211@gmail.com>
2023-03-17 11:34:45 -0300

    First app using swift UI, made based on a youtube tutorial

--- RandomPhoto/ContentView.swift
+++ RandomPhoto/ContentView.swift
@@ -7,15 +7,76 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+class ViewModel: ObservableObject {
+    @Published var image: Image?
+    
+    func fetchNewImage() {
+        guard let url = URL(string: "https://random.imagecdn.app/500/500") else {
+            return
         }
-        .padding()
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            data, _, _ in
+            guard let data = data else {
+                return
+            }
+            
+            DispatchQueue.main.sync {
+                guard let uiImage = UIImage(data: data) else {
+                    return
+                }
+                self.image = Image(uiImage: uiImage)
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+                Spacer()
+                
+                if let image = viewModel.image {
+                    ZStack {
+                        image
+                            .resizable()
+                            .foregroundColor(.green)
+                            .frame(width: 200, height: 200)
+                            .padding()
+                    }
+                    .frame(width: UIScreen.main.bounds.width / 1.2, height: UIScreen.main.bounds.width / 1.2)
+                    .background(.pink)
+                    .cornerRadius(8)
+                    
+                } else {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .foregroundColor(.cyan)
+                        .frame(width: 200, height: 200)
+                        .padding()
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    viewModel.fetchNewImage()
+                }, label: {
+                    Text("New Image!")
+                        .frame(width: 250, height: 55)
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .cornerRadius(8)
+                        .padding()
+                })
+            }
+            .navigationTitle("Random Photo")
+        }
     }
 }
 
